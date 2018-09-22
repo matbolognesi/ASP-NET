6c35ae855cf1caa84dcfefc22b46974be6e9e780
matbolognesi <matbolognesi@outlook.com>
2018-09-22 18:32:12 -0300

    Exemplo de formulario web usando web forms em asp net

---
+++ Exemplo de Formulario/Exemplo de Formulario/Formulario2.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Exemplo_de_Formulario
+{
+    public partial class Formulario2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Params["parametro"] != null)
+            {
+                Label1.Text = Request.Params["parametro"];
+            }
+
+        }
+    }
+}
